e9c293e81cf4ec51ad3f610d86975f37535f6387
kmd <kamehaya1295at@gmail.com>
2023-04-28 11:16:17 +0900

    “ポートフォリオ”

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+$array
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+<link rel="stylesheet" href="style.css">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ポートフォリオ 自己紹介(亀田)</title>
+</head>
+<body>
+    <div class="Header">自己紹介クイズ</div>
+    <div><p>私の自己紹介をクイズ形式で行います！</p>
+    <p>答えは考えないで適当に答えてください！</p></div>
+    
+
+    <div class="sidenavi"></div>
+    <h2>サイドナビ</h2>
+<nav>
+<ul>
+<li>サイドナビ</li>
+<li>サイドナビ</li>
+<li>サイドナビ</li>
+<li>サイドナビ</li>
+<li>サイドナビ</li>
+<li>サイドナビ</li>
+</ul>
+</nav>
+</div>
+
+
+
+</body>
+</html>
